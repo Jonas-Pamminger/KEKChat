8434ee9ff34438aa4512e86d02999e3b6580eadf
Andrei Stana <andreistana1412@gmail.com>
2021-06-19 22:45:10 +0200

    Created the log in page

---
+++ KEKChat/Controllers/DatabaseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KEKChat.Controllers
+{
+    public class DatabaseController
+    {
+        public static bool CheckInformation(string username, string password)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Andrei\Desktop\LoginDatabase.mdf;Integrated Security=True;Connect Timeout=30");
+
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From [Table] Where [USERNAME]='" + username + "' and [PASSWORD]='" + password + "'", con);
+
+            DataTable dt = new DataTable();
+
+            sda.Fill(dt);
+
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
