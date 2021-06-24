43c12b2d0f57927f7a1613e1ba8b9a67fd46835f
Andrei Stana <andreistana1412@gmail.com>
2021-06-25 00:33:43 +0200

    Changed the root

--- KEKChat/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Login.razor.g.cs
+++ KEKChat/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Login.razor.g.cs
@@ -13,70 +13,70 @@ namespace KEKChat.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\IF200195\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 1 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\IF200195\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 2 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\IF200195\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 3 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\IF200195\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 4 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\IF200195\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 5 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\IF200195\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 6 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\IF200195\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 7 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\IF200195\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 8 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using KEKChat;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\IF200195\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 9 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using KEKChat.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\IF200195\Source\Repos\RoKyYy1412\KEKChat\KEKChat\Pages\Login.razor"
+#line 3 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\Pages\Login.razor"
 using Controllers;
 
 #line default
@@ -91,7 +91,7 @@ using Controllers;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 27 "C:\Users\IF200195\Source\Repos\RoKyYy1412\KEKChat\KEKChat\Pages\Login.razor"
+#line 26 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\Pages\Login.razor"
        
 
     string _username;
@@ -108,6 +108,7 @@ using Controllers;
 
         if (DatabaseController.CheckInformation(_username, _password) == true)
         {
+            clicked = false;
             NavManager.NavigateTo("/chatroom");
         }
         else
--- KEKChat/obj/Release/netcoreapp3.1/RazorDeclaration/Pages/Index.razor.g.cs
+++ KEKChat/obj/Release/netcoreapp3.1/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,63 +13,63 @@ namespace KEKChat.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\IF200195\source\repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 1 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\IF200195\source\repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 2 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\IF200195\source\repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 3 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\IF200195\source\repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 4 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\IF200195\source\repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 5 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\IF200195\source\repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 6 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\IF200195\source\repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 7 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\IF200195\source\repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 8 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using KEKChat;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\IF200195\source\repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
+#line 9 "C:\Users\Andrei\Source\Repos\RoKyYy1412\KEKChat\KEKChat\_Imports.razor"
 using KEKChat.Shared;
 
 #line default
